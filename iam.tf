f8aeb57957e668b5627707ef049ffffd773ec90f
nikolovatanass <atanas_nikolov@flutterint.com>
2023-06-02 11:16:48 +0300

    adding main fails

---
+++ iam.tf
@@ -0,0 +1,35 @@
+# Creating IAM role -----------------------------------------------------------
+resource "aws_iam_role" "code_build" {
+  name = "ssm-mgmt"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principals = {
+            type = "Service"
+            identifiers = ["cloudbuild.amazon.com"]
+        }
+      },
+    ]
+  })
+  tags = {
+    name = "role"
+  }
+}
+
+# Attaching instance policy to role -------------------------------------------
+resource "aws_iam_role_policy_attachment" "code_build_attach" {
+  role       = aws_iam_role.code_build.id
+  policy_arn = var.code_build_policy
+}
+
+# # Creating instance profile with the role -------------------------------------
+# resource "aws_iam_instance_profile" "iam_instance_profile" {
+#   name = "instance-profile"
+#   role = aws_iam_role.ssm_selfmade.name
+#   tags = {
+#     name = "profile"
+#   }
+# }
